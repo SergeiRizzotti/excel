63d84ec41ca4a4200f95fc9db97022df4ff19960
Sergei <sergei.rizzotti@gmail.com>
2020-08-31 17:39:44 +0300

    Finish redux and table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,8 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
+import {debounce} from '@core/utils';
+import {changeTitle} from '@/redux/actions';
+import {defaultTitle} from '@/constants';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header';
@@ -6,13 +10,19 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
+      listeners: ['input'],
       ...options,
     });
   }
 
+  prepare() {
+    this.onInput = debounce(this.onInput, 300);
+  }
+
   toHTML() {
+    const title = this.store.getState().title || defaultTitle;
     return `
-    <input class="input" type="text" value="Новая таблица" />
+    <input class="input" type="text" value="${title}" />
     <div class="button">
       <span class="material-icons">
         exit_to_app
@@ -24,5 +34,10 @@ export class Header extends ExcelComponent {
       </span>
     </div>
     `;
+  }
+
+  onInput(event) {
+    const $target = $(event.target);
+    this.$dispatch(changeTitle($target.text()));
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,8 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
-    this.unsubscribers = [];
+    this.subscribe = options.subscribe || [];
+    this.store = options.store;
 
     this.prepare();
   }
@@ -21,8 +22,17 @@ export class ExcelComponent extends DomListener {
   }
 
   $on(event, fn) {
-    const unsub = this.emitter.subscribe(event, fn);
-    this.unsubscribers.push(unsub);
+    this.emitter.subscribe(event, fn);
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key);
   }
 
   init() {
